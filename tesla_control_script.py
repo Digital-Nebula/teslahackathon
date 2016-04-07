d4f7f210d66e6d4a2f4113cb4a9b77ae12e025de
Rick Fish <rick.fish@capco.com>
2016-04-07 10:51:31 +0100

    Updated to add more information and clean up script
    Second push

--- tesla_control_script.py
+++ tesla_control_script.py
@@ -3,9 +3,12 @@
  
 import os
 import teslajson
- 
+
+#--------Setup vars---------
 TESLA_EMAIL = os.environ['TESLA_EMAIL']
 TESLA_PASSWORD = os.environ['TESLA_PASSWORD']
+TESLA_ACCESS_TOKEN = os.environ['TESLA_ACCESS_TOKEN']
+TESLA_CAR_NAME = os.environ['TESLA_CAR_NAME']
 
 #-------Helper Functions------------ 
 def establish_connection(token=None):
@@ -24,30 +27,44 @@ def issue_command(c, car, comm):
           d = v.command(comm)
 	  return d
 
+def print_info(name, data):
+	print '\n\n' + name + '\n-----------------------------'
+	for x in data:
+		print x + ' : ' + str(data[x])
+	return 0
+
 #---------Main Flow------------ 
 #Connect to the car (using credentials in environment)
-c = establish_connection()
+c = establish_connection(TESLA_ACCESS_TOKEN)
 
-#Get the array of current data for the car (vehicle and charge state)
-vehicle_data = get_car_data(c, "Blue Thunder", "vehicle_state")
-charge_status = get_car_data(c, "Blue Thunder", "charge_state")
+#Get the array of current data for the car name passed - performs 1 rest call per request
+vehicle_data = get_car_data(c, TESLA_CAR_NAME, "vehicle_state")
+charge_data = get_car_data(c, TESLA_CAR_NAME, "charge_state")
+drive_data = get_car_data(c, TESLA_CAR_NAME, "drive_state")
+climate_data = get_car_data(c, TESLA_CAR_NAME, "climate_state")
+gui_data = get_car_data(c, TESLA_CAR_NAME, "gui_settings")
 
-#Get the api list of commands (currently supported)
-#print "Vehicle State Data:\n" + "\n".join(vehicle_data)
-#print "Charge State Commands:\n" + "\n".join(charge_status)
+print 'Basic Car Data, id, vin etc.\n---------'
+for a in c.vehicles:
+	for b in a:
+		print b + ' : ' + str(a[b])
 
-#Lets print a few things
-print "Mileage=" + str(vehicle_data["odometer"])
-print "Is Locked?=" + str(vehicle_data["locked"])
+"""Print some stuff out"""
+print_info('Vehicle Data', vehicle_data);
+print_info('Charge Data', charge_data);
+print_info('Drive State Data', drive_data);
+print_info('Climate Data', climate_data);
+print_info('GUI Data', gui_data);
 
 #What is the estimated remaining range
-print "Est_battery_range=" + str(charge_status["est_battery_range"])
-#EU Vehicle query
-print "EU Vehicle=" + str(charge_status["eu_vehicle"])
-print "Managed User Start Time=" + str(charge_status["managed_charging_start_time"])
+print '\n\nSample Particular Output\n-------------------'
+print "Est_battery_range=" + str(charge_data["est_battery_range"])
+print "Managed User Start Time=" + str(charge_data["managed_charging_start_time"])
 
+""" Commented out for now
 #Commands to try
 #Set Standard 80% charge rate
-#issue_command(c, "Blue Thunder", "charge_standard")
+#issue_command(c, TESLA_CAR_NAME, "charge_standard")
 #Set max 100% charge rate
-#issue_command(c, "Blue Thunder", "charge_max_range")
+#issue_command(c, TESLA_CAR_NAME, "charge_max_range")
+"""
